d6f9d7aaae81981ff3e948cd270434849eb906b6
KIMMINJI <amber_lucy@naver.com>
2025-07-16 13:45:22 +0900

    fix : 의존성 수정

--- src/main/java/org/example/qpin/global/jwt/JWTUtil.java
+++ src/main/java/org/example/qpin/global/jwt/JWTUtil.java
@@ -2,12 +2,11 @@ package org.example.qpin.global.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
@@ -17,11 +16,13 @@ public class JWTUtil {
     private final SecretKey secretKey;
 
     public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
-        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), SignatureAlgorithm.HS256.getJcaName());
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
     }
 
     private Claims getClaims(String token) {
-        return Jwts.parserBuilder()
+        return Jwts
+                .parserBuilder()
                 .setSigningKey(secretKey)
                 .build()
                 .parseClaimsJws(token)
@@ -53,13 +54,14 @@ public class JWTUtil {
     }
 
     public String createJwt(String category, String username, String role, Long expiredMs) {
-        return Jwts.builder()
+        return Jwts
+                .builder()
                 .claim("category", category)
                 .claim("username", username)
                 .claim("role", role)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
-                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .signWith(secretKey) // 0.12.x부터는 알고리즘 자동 인식
                 .compact();
     }
 }
